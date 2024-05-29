3cc146834852b8f19cd1459d41d72ec2051d96ea
lifeofstephanie <stephanieanyanwu605@gmail.com>
2024-05-29 09:17:21 +0000

    done witj task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,8 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse) {
-    // eslint-disable-next-line no-unused-vars
-    const task = true;
-    //eslint-disable-next-line no-used-vars
+  if (trueOrFalse) { // eslint-disable-next-line no-unused-vars
+    const task = true; //eslint-disable-next-line no-used-vars
     const task2 = false;
   }
 
