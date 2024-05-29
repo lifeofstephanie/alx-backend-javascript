7223d2dd094d3d838b7eb67c8321d7387d6556fa
lifeofstephanie <stephanieanyanwu605@gmail.com>
2024-05-29 10:03:36 +0000

    done

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -9,7 +9,6 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
   budget[`income-${getCurrentYear()}`] = income;
   budget[`gdp-${getCurrentYear()}`] = gdp;
   budget[`capita-${getCurrentYear()}`] = capita;
-
+  };
   return budget;
-  }
 }
