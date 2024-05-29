2acc93db0235a8a3ec12b6c1f6578981bf96e348
lifeofstephanie <stephanieanyanwu605@gmail.com>
2024-05-29 14:49:31 +0000

    done

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  const body = { status:200, body:'success' };
+  const body = { status: 200, body: 'success' };
 
   return promise;
   .then(() => body);
-  .catch(() => new Error);
+  .catch(() => new Error());
   .finally(() => {
     console.log('Got a response from the API');
   });
