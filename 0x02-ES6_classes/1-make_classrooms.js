137bec43643352f892fe48e90a4234a1d92bb628
lifeofstephanie <stephanieanyanwu605@gmail.com>
2024-05-30 13:21:45 +0000

    done

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,3 @@
-import ClassRoom from './0-classroom.js';
-
 export default function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(24)];
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
